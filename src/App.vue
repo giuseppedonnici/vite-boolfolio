4dec7820388a8e9e0a082cd7375e44bd44a199f5
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-06-13 17:21:33 +0200

    Add: template in app.vue to show projects

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,44 @@
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    this.getProjects();
+  },
+  methods: {
+    getProjects() {
+      axios.get('http://localhost:8000/api/projects').then(resp => {
+        this.projects = resp.data.result;
+      })
+    }
+  }
 }
 </script>
 
 
 <template>
-  <h1>Ciaooo</h1>
+  <div class="container">
+    <h2>Lista dei progetti</h2>
+    <div class="row row-cols-3 g-3">
+      <div class="col " v-for="project in projects" :key="project.id">
+        <div class="card h-100">
+        <img v-if="project.image" :src="`http://localhost:8000/storage/${project.image}`" class="card-img-top" :alt="project.title">
+        <div v-else>
+          Nessuna immagine presente
+        </div>
+          <div class="card-body">
+            <h5>{{ project.title }}</h5>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
