5491599064dcf9339911d763a489fa69b7bd7116
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-06-15 21:52:29 +0200

    Add: header with router-link

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,39 @@
 <script>
 export default {
-    name: "AppHeader"
-}
+  name: "AppHeader",
+};
 </script>
 
 <template>
-    <header>
-        <h5>Header</h5>
-    </header>
+  <header>
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Boolfolio</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link :to="{name: 'home'}" class="nav-link active" aria-current="page">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link :to="{name: 'projects'}" class="nav-link active" aria-current="page">Projects</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link :to="{name: 'about'}" class="nav-link active" aria-current="page">About us</router-link>
+            </li>
+           </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
 </template>
