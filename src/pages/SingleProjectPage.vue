1d0abfb873442ac387ae61c75463c9a27adfe1ed
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-06-17 22:25:07 +0200

    SingleProjectCard: Type and Technologies

--- src/pages/SingleProjectPage.vue
+++ src/pages/SingleProjectPage.vue
@@ -22,6 +22,11 @@ export default {
   <div class="container">
     <div class="project-details" v-if="project">
         <h2>{{ project.title }}</h2>
+        <h6>Tipologia: {{ project.type ? project.type.name : "Nessuna tipologia assegnata" }}</h6>
+        <p v-if="project.technology > 0">
+            <span class="me-2" v-for="technology in project.technologies">{{ technology.name }}</span>
+        </p>
+        <p v-else>{{ "Nessuna tecnologia assegnata" }}</p>
         <p>{{ project.description }}</p>
     </div>
   </div>
